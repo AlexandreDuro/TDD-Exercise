84f6a8fd3f5746dd1bfe77a19072487a5f2bb856
alexandreduro <alexandreduro11@gmail.com>
2025-12-09 11:58:42 +0100

    [TDD] RED: Add test for Laboratory with valid substances

--- tests/test_laboratory.py
+++ tests/test_laboratory.py
@@ -6,4 +6,8 @@ class TestLaboratoryInitialization:
     def test_create_laboratory_with_empty_list(self):
         """Should create a laboratory with an empty list of substances."""
         lab = Laboratory([])
+        assert lab is not None   
+
+    def test_create_laboratory_with_valid_substances(self):
+        lab = Laboratory(["Water", "Ethanol"]
         assert lab is not None
