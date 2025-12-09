2f979d9ccc0b650351e02968df1529d3de0c7c25
alexandreduro <alexandreduro11@gmail.com>
2025-12-09 11:51:37 +0100

    [TDD] RED: Add test for Laboratory creation with empty list

---
+++ tests/test_laboratory.py
@@ -0,0 +1,9 @@
+import pytest
+from src.laboratory import Laboratory
+
+
+class TestLaboratoryInitialization:
+    def test_create_laboratory_with_empty_list(self):
+        """Should create a laboratory with an empty list of substances."""
+        lab = Laboratory([])
+        assert lab is not None
